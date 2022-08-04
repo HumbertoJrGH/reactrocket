55329ea6bf7c8f0a23ffcd84605ceabb5fd2bf19
HumbertJrGH <dev.humbertojr@gmail.com>
2022-08-04 18:43:11 -0300

    atualização

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,51 @@ import './styles/global.css';
 
 import { Sidebar } from './components/Sidebar.jsx';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      name: "Bernarde",
+      picture: "./src/img/profile.webp",
+      role: "Guarda"
+    },
+    published: new Date("2022-08-01 18:03:53"),
+    content: [
+      {type: "paragraph", content: "Vincenzo, onde está você?"},
+      {type: "paragraph", content: "Passei a noite toda cozinhando um pato com vinho."},
+      {type: "link", content: "clique"}
+    ],
+  },
+  {
+    id: 2,
+    author: {
+      name: "Alejandro",
+      picture: "./src/img/profile.webp",
+      role: "Guarda"
+    },
+    published: new Date("2022-08-01 19:13:15"),
+    content: [
+      {type: "paragraph", content: "Vincenzo, onde está você?"},
+      {type: "paragraph", content: "Passei a noite toda cozinhando um pato com vinho."},
+      {type: "link", content: "clique"}
+    ],
+  },
+  {
+    id: 3,
+    author: {
+      name: "Martin",
+      picture: "./src/img/profile.webp",
+      role: "Guarda"
+    },
+    published: new Date("2022-08-01 20:47:17"),
+    content: [
+      {type: "paragraph", content: "Vincenzo, onde está você?"},
+      {type: "paragraph", content: "Passei a noite toda cozinhando um pato com vinho."},
+      {type: "link", content: "clique"}
+    ],
+  },
+]
+
 function App() {
 
   return (
@@ -14,16 +59,15 @@ function App() {
       <div className={style.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Bernarde F. Castagno"
-            cargo="Espadachim"
-            content="Meu amor de toda a vida!adgadgadg"
-          />
-          <Post
-            author="Alejandro Barrillo"
-            cargo="Mercenário"
-            content="Quem é esse loirinho nojento?"
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                published={post.published}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
